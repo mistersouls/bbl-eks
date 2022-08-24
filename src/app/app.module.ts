4a97db0977dce8ab0f3485138ea0a3dafa11545a
Souleymane BA <soulsmister@gmail.com>
2022-08-24 05:12:41 +0200

    create empty slides

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlidesComponent } from './slides/slides.component';
+import { SwiperModule } from 'swiper/angular';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlidesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
